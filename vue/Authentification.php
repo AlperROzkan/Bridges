2621f7a1accf14d08aefe53e7175a11056f59fdd
AlperROzkan <alper.ozkan@etu.univ-nantes.fr>
2018-11-28 09:43:17 +0100

    Regroupement de l'authentification dans un fichier

---
+++ vue/Authentification.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<br/>
+<br/>
+<form method="post" action="index.php">
+    Entrer votre pseudo  <input type="text" name="pseudo"/>
+    </br>
+    Entrer votre mdp:<input type="password" name="mdp" size="20"/>
+    </br>
+    <br/>
+    <br/>
+    <input type="submit" value="Envoyer"/>
+    <input type="reset" value="Annuler"/>
+    <br/>
+</form>
+<br/>
+<br/>
--- vue/vue.php
+++ vue/vue.php
@@ -1,28 +1,8 @@
 <?php
 
 class Vue{
-
-function demandePseudo(){
-   
-?>
-    <html>
-    <body>
-    <br/>
-    <br/>
-    <form method="post" action="index.php">
-    Entrer votre pseudo  <input type="text" name="pseudo"/>
-    </br>
-    Entrer votre mdp:<input type="password" name="mdp" size="20"/>
-    </br>
-    <br/>
-    <br/>
-    <input type="submit" value="Envoyer"/>
-    <input type="reset" value="Annuler"/>
-    <br/>
-    </form>
-    <br/>
-    <br/>
-<?php
+    function demandePseudo(){
+        require "Authentification.php";
     }
 }
 ?>
