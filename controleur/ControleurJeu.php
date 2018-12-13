a04c30216ec6577fa3c498fb4e3576cd79c066e2
Robin Eliott <liopty@gmail.com>
2018-12-13 22:24:26 +0100

    dbfh

--- controleur/ControleurJeu.php
+++ controleur/ControleurJeu.php
@@ -45,7 +45,7 @@ class ControleurJeu
                 echo "<br><br>";
                 $this->vue->commenceJeu($_SESSION['villes']);
                 echo "<br><br>";
-
+                var_dump($_SESSION['villes']->getToutesVillesLiees());
                 unset($_SESSION['actif']);
 
             }
