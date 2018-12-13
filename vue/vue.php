1edd57acec8a32f4818ff443d1f391a4f84f994c
Robin Eliott <liopty@gmail.com>
2018-12-13 18:20:47 +0100

    ajout d'une methode pour avoir les coordonnées d'une ville pas son id + supreesion d'une fonction de test

--- vue/vue.php
+++ vue/vue.php
@@ -18,17 +18,6 @@ class Vue
     {
         require "Jeu.php";
     }
-
-    function test($ville1, $ville2)
-    {
-       var_dump($ville1);
-       echo "<br><br>";
-       var_dump($ville2);
-
-       
-    }
-
-
 }
 
 ?>
