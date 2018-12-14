1cb20730591134d96c240fc9cc5eb4e60733a8aa
Robin Eliott <liopty@gmail.com>
2018-12-14 21:49:30 +0100

    ff

--- modele/Villes.php
+++ modele/Villes.php
@@ -176,7 +176,7 @@ class Villes
                 while ($villeCoord[1] < $coord[1]-1) {
                   $villeCoord[1] ++;
                   if (!in_array($villeCoord, $res)){
-                    $res[] = $villeCoord+"h1";
+                    $res[] = array_push($villeCoord, "h1");
                   }
                 }
               }
@@ -191,7 +191,7 @@ class Villes
                 while ($villeCoord[0] < $coord[0]-1) {
                   $villeCoord[0] ++;
                   if (!in_array($villeCoord, $res)){
-                    $res[] = $villeCoord+"v1";
+                    $res[] = array_push($villeCoord,"v1");
                   }
 
                 }
